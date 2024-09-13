708a3cff5aeb59dfeba5c2276cdfe9a6089eccbb
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-13 09:25:39 +0200

    create product list html v1

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
-
-
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     ProductListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ]
 })
 export class ProductModule { }
