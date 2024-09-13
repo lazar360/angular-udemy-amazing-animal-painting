23404269856b39a7d34b99d0c1cbfd57a1452ae0
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-13 12:22:43 +0200

    add total cart price

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
-import { CartService } from '../cart.service';
-import { Product } from 'src/app/models/product';
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';  
+import { CartService } from '../cart.service';  
+import { Product } from 'src/app/models/product';  
 
-@Component({
-  selector: 'app-cart-view',
-  templateUrl: './cart-view.component.html',
-  styleUrls: ['./cart-view.component.css']
-})
-export class CartViewComponent implements OnInit{
+@Component({  
+  selector: 'app-cart-view',  
+  templateUrl: './cart-view.component.html',  
+  styleUrls: ['./cart-view.component.css']  
+})  
+export class CartViewComponent implements OnInit {  
 
-  cartItems : Product[] = [];
+  cartItems: Product[] = [];  
+  totalPrice: number = 0;  
 
-  constructor(private cartService : CartService){}
-  
-  ngOnInit(): void {
-      this.cartService.getCartItems().subscribe(data => this.cartItems = data);
-  }
+  constructor(private cartService: CartService) { }  
 
+  ngOnInit(): void {  
+    this.cartService.getCartItems().subscribe(data => {  
+      this.cartItems = data;  
+      this.totalPrice = this.getTotalPrice(); // Calculate the total price after loading cart items  
+    });  
+  }  
+
+  getTotalPrice(): number {  
+    let total = 0;  
+    this.cartItems.forEach(item => {  
+      total += item.price; // Accumulate the total price  
+    });  
+    return total; // Return the calculated total price  
+  }  
 }
