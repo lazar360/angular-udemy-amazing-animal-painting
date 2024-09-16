491a67e42f6ae96d7d29e9098eef97564981479c
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-16 10:00:04 +0200

    add search bar to product list

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -11,30 +11,38 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  filteredProducts: Product[] = [];
 
   constructor(
     private productService: ProductService,
     private cartService: CartService,
-    private snackbar : MatSnackBar
+    private snackbar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
-    this.productService
-      .getProducts()
-      .subscribe((data) => (this.products = data));      
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+      this.filteredProducts = data;
+    });
   }
 
   addToCart(product: Product): void {
-    this.cartService.addToCart(product).subscribe(
-      {
-        next: () => {
-          this.snackbar.open("Product added to cart!", "", {
-            duration : 2000,
-            horizontalPosition : 'right',
-            verticalPosition : 'top'
-          });
-        }
-      }
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackbar.open('Product added to cart!', '', {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+      },
+    });
+  }
+
+  applyFilter(event: Event): void {
+    let searchTerm = (event.target as HTMLInputElement).value;
+    searchTerm = searchTerm.toLowerCase();
+    this.filteredProducts = this.products.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm)
     );
   }
 }
